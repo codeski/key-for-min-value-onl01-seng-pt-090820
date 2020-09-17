33b33d364903f1a17bd185042eba3c675b3340e9
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-17 02:40:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |key, value|
+  name_hash.each do |key, value|
     if value == 1 || value == 10
       return key
     else
-      nil
+      
     end
   end
 end
