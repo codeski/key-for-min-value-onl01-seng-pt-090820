f0916ea9d2a7abda133aeda9bb85a8acbc39e44b
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-17 03:55:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -18,7 +18,9 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |key, value|
     if min_value == nil || value < min_value
-      return min_key
+  		min_value = value
+      min_key = key
+    end
     end
   end
 end
